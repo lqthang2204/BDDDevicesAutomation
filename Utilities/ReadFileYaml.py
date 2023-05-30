4f2a0a40b3081d0ff98d3506ac388ee374d61ab4
lqthang2204 <quangthangle2204@gmail.com>
2023-05-30 20:09:15 +0700

    updated
    step2

--- Utilities/ReadFileYaml.py
+++ Utilities/ReadFileYaml.py
@@ -5,22 +5,20 @@ from pathlib import Path
 
 
 class read_file_yaml:
-    def __init__(self, dict_yaml):
-       self.dict_yaml = dict_yaml
 
     def get_dict_path_yaml(self):
+        dict_yaml = dict()
         print("reading path yaml")
         file_path = sys.path[1] + '/resources/pages/*/*.yaml'
         # Find all files with the .txt extension in the specified directory
         files = glob.glob(file_path)
         for file in files:
             path, file_name = os.path.split(file)
-            self.dict_yaml[file_name] = path
-        print(self.dict_yaml)
-        return self.dict_yaml
+            dict_yaml[file_name] = path
+        # print(dict_yaml.get("pageOrange.yaml"))
+        return (dict_yaml)
 
 
 
-dict_yaml = {}
-obj = read_file_yaml(dict_yaml)
+obj = read_file_yaml()
 obj.get_dict_path_yaml()
--- features/steps/execure_script.py
+++ features/steps/execure_script.py
@@ -1,9 +1,16 @@
+import configparser
 from behave import *
 from selenium import webdriver
 @given(u'I navigate to "{url}"')
 def launchBrowser(context, url):
-    context.driver = webdriver.Chrome()
-    context.driver.get(url)
+    file = open("config.ini",'r')
+    config = configparser.RawConfigParser(allow_no_value=True)
+    config.read_file(file)
+    if config.get("drivers_config","auto_download_driver"):
+        driver = webdriver.Chrome(executable_path=config.get("drivers_config","driver_version"))
+    else:
+        driver = webdriver.Chrome()
+    driver.get(url)
     print("done")
 
 # @given(u'I change the page spec to {page}')
