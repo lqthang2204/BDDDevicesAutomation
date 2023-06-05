21b63e3e6b8409cb698ff43d0f7c7b8a3aea7b15
lqthang2204 <quangthangle2204@gmail.com>
2023-06-05 15:48:28 +0700

    update
    final

--- features/environment.py
+++ features/environment.py
@@ -2,11 +2,14 @@ from Utilities.ReadFileYaml import ManagementFile
 import configparser
 from selenium import webdriver
 import os
+import chromedriver_autoinstaller
+
 def before_all(context):
     config_file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'config.ini')
     file = open(config_file_path, 'r')
     config = configparser.RawConfigParser(allow_no_value=True)
     config.read_file(file)
+    chromedriver_autoinstaller.install()
     if config.get("drivers_config", "auto_download_driver") == 'False':
         context.driver = webdriver.Chrome(executable_path=os.path.dirname(os.path.dirname(__file__))+"\\"+config.get("drivers_config", "driver_version"))
     else:
