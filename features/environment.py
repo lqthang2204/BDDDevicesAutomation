8df4fc8cdb7b7eb6b4359a7b140fe050e4a1c903
Thang Le <quangthangle1402@gmail.com>
2023-06-10 14:38:45 +0700

    update file config
    updated

--- features/environment.py
+++ features/environment.py
@@ -3,17 +3,22 @@ import configparser
 from selenium import webdriver
 import os
 import chromedriver_autoinstaller
+from selenium.webdriver.chrome.options import Options
 
 def before_all(context):
     config_file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'config.ini')
     file = open(config_file_path, 'r')
     config = configparser.RawConfigParser(allow_no_value=True)
+    chrome_option = Options()
+    chrome_option.add_argument("--headless")
+    chrome_option.add_argument("--no-sandbox")
+    chrome_option.add_argument("--disable-dev-shm-usage")
     config.read_file(file)
     chromedriver_autoinstaller.install()
     if config.get("drivers_config", "auto_download_driver") == 'False':
-        context.driver = webdriver.Chrome(executable_path=os.path.dirname(os.path.dirname(__file__))+"\\"+config.get("drivers_config", "driver_version"))
+        context.driver = webdriver.Chrome(executable_path=os.path.dirname(os.path.dirname(__file__))+"\\"+config.get("drivers_config", "driver_version"), options=chrome_option)
     else:
-        context.driver = webdriver.Chrome()
+        context.driver = webdriver.Chrome(options=chrome_option)
     print("----------------------Reading file config-----------------------------")
     context.dict_yaml = ManagementFile.get_dict_path_yaml()
     context.wait = config.get("drivers_config", "wait")
