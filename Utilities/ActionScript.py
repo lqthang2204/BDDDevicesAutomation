d67ac986d37eda93e704f7a9b598dd8b0fa71c14
Thang Le <quangthangle1402@gmail.com>
2023-06-18 10:20:59 +0700

    fix bug not execute action
    updated

--- Utilities/ActionScript.py
+++ Utilities/ActionScript.py
@@ -110,64 +110,64 @@ class ManagementFile:
                             else:
                                 value = row["Value"]
                             break
-                    element_page = action_elements.get_element()
-                    type_action = action_elements.get_inputType()
-                    locator = self.get_locator_from_action(element_page, "WEB")
-                    element = self.get_element_by(locator.type, driver, locator.value)
-                    if action_elements.get_condition() is not None and action_elements.get_timeout() is not None:
-                        try:
-                            if action_elements.get_condition() == "ENABLED":
-                                WebDriverWait(driver, action_elements.get_timeout()).until(
-                                    ec.element_to_be_clickable(element))
-                            elif action_elements.get_condition() == "NOT_ENABLED":
-                                WebDriverWait(driver, action_elements.get_timeout()).until_not(
-                                    ec.element_to_be_clickable(element))
-                            elif action_elements.get_condition() == "DISPLAYED":
-                                WebDriverWait(driver, action_elements.get_timeout()).until(
-                                    ec.presence_of_element_located(element))
-                            elif action_elements.get_condition() == "NOT_DISPLAYED":
-                                WebDriverWait(driver, action_elements.get_timeout()).until(
-                                    ec.presence_of_element_located(element))
-                            elif action_elements.get_condition() == "EXISTED":
-                                elements = self.get_list_element_by(locator.type, driver, locator.value)
-                                WebDriverWait(driver, action_elements.get_timeout()).until(
-                                    lambda driver: len(elements) > int(0))
-                            elif action_elements.get_condition() == "NOT_EXISTED":
-                                elements = self.get_list_element_by(locator.type, driver, locator.value)
-                                WebDriverWait(driver, action_elements.get_timeout()).until_not(
-                                    lambda driver: len(elements) > int(0))
-                            elif action_elements.get_condition() == "SELECTED":
-                                WebDriverWait(driver, action_elements.get_timeout()).until(
-                                    ec.element_located_to_be_selected(element))
-                            elif action_elements.get_condition() == "NOT_SELECTED":
-                                WebDriverWait(driver, action_elements.get_timeout()).until_not(
-                                    ec.element_located_to_be_selected(element))
+                element_page = action_elements.get_element()
+                type_action = action_elements.get_inputType()
+                locator = self.get_locator_from_action(element_page, "WEB")
+                element = self.get_element_by(locator.type, driver, locator.value)
+                if action_elements.get_condition() is not None and action_elements.get_timeout() is not None:
+                    try:
+                        if action_elements.get_condition() == "ENABLED":
+                            WebDriverWait(driver, action_elements.get_timeout()).until(
+                                ec.element_to_be_clickable(element))
+                        elif action_elements.get_condition() == "NOT_ENABLED":
+                            WebDriverWait(driver, action_elements.get_timeout()).until_not(
+                                ec.element_to_be_clickable(element))
+                        elif action_elements.get_condition() == "DISPLAYED":
+                            WebDriverWait(driver, action_elements.get_timeout()).until(
+                                ec.presence_of_element_located(element))
+                        elif action_elements.get_condition() == "NOT_DISPLAYED":
+                            WebDriverWait(driver, action_elements.get_timeout()).until(
+                                ec.presence_of_element_located(element))
+                        elif action_elements.get_condition() == "EXISTED":
+                            elements = self.get_list_element_by(locator.type, driver, locator.value)
+                            WebDriverWait(driver, action_elements.get_timeout()).until(
+                                lambda driver: len(elements) > int(0))
+                        elif action_elements.get_condition() == "NOT_EXISTED":
+                            elements = self.get_list_element_by(locator.type, driver, locator.value)
+                            WebDriverWait(driver, action_elements.get_timeout()).until_not(
+                                lambda driver: len(elements) > int(0))
+                        elif action_elements.get_condition() == "SELECTED":
+                            WebDriverWait(driver, action_elements.get_timeout()).until(
+                                ec.element_located_to_be_selected(element))
+                        elif action_elements.get_condition() == "NOT_SELECTED":
+                            WebDriverWait(driver, action_elements.get_timeout()).until_not(
+                                ec.element_located_to_be_selected(element))
+                        else:
+                            assert False, "Not support condition"
+                        if type_action.__eq__("click"):
+                            if element.get_attribute("disabled") is None:
+                                element.click()
                             else:
-                                assert False, "Not support condition"
-                            if type_action.__eq__("click"):
-                                if element.get_attribute("disabled") is None:
-                                    element.click()
-                                else:
-                                    WebDriverWait(driver, action_elements.get_timeout).until_not(
-                                        ec.element_attribute_to_include(
-                                            self.get_locator_for_wait(locator.type, locator.value), "disabled"))
-                                    element.click()
-                            elif type.__eq__("text"):
-                                element.send_keys(value)
-                        except:
-                            assert True, "due to have Condition and time out by pass step"
-                    elif action_elements.get_condition() is not None and action_elements.get_timeout() is None:
-                        try:
-                            self.process_execute_action(driver, wait, element, type_action, value,
-                                                        locator)
-                        except:
-                            assert False, "Failed at action step"
-                    else:
-                        try:
-                            self.process_execute_action(driver, wait, element, type_action, value,
-                                                        locator)
-                        except:
-                            assert False, "Failed at action step"
+                                WebDriverWait(driver, action_elements.get_timeout).until_not(
+                                    ec.element_attribute_to_include(
+                                        self.get_locator_for_wait(locator.type, locator.value), "disabled"))
+                                element.click()
+                        elif type.__eq__("text"):
+                            element.send_keys(value)
+                    except:
+                        assert True, "due to have Condition and time out by pass step"
+                elif action_elements.get_condition() is not None and action_elements.get_timeout() is None:
+                    try:
+                        self.process_execute_action(driver, wait, element, type_action, value,
+                                                    locator)
+                    except:
+                        assert False, "Failed at action step"
+                else:
+                    try:
+                        self.process_execute_action(driver, wait, element, type_action, value,
+                                                    locator)
+                    except:
+                        assert False, "Failed at action step"
         else:
             assert False, "Not Found Action in page yaml"
 
