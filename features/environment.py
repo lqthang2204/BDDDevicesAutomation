17402b212d5d36b94bd2ef55b38a75ae5095a0ab
Thang Le <quangthangle1402@gmail.com>
2023-07-23 17:15:57 +0700

    update execute with multi-browser
    updated

--- features/environment.py
+++ features/environment.py
@@ -3,13 +3,18 @@ import configparser
 from selenium import webdriver
 import os
 import chromedriver_autoinstaller
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as chrome_option
+from selenium.webdriver.firefox.options import Options as firefox_option
+from selenium.webdriver.safari.options import Options as safari_option
 from Utilities.read_configuration import read_configuration
 from Configuration.stage import stage
 from Configuration.devices import devices
 from Configuration.configuration_env import environment_config
 from appium.options.android import UiAutomator2Options
 from appium.webdriver.appium_service import AppiumService
+from selenium.webdriver.chrome.service import Service as chrome_service
+from selenium.webdriver.firefox.service import Service as firefox_service
+from selenium.webdriver.safari.service import Service as safari_service
 import datetime
 import appium
 import logging
@@ -22,10 +27,13 @@ def before_all(context):
     config_file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'config_env.ini')
     file = open(config_file_path, 'r')
     config = configparser.RawConfigParser(allow_no_value=True)
-    chrome_option = Options()
     config.read_file(file)
     platform = config.get("drivers_config", "platform")
     stage_name = config.get("drivers_config", "stage")
+    if config.has_option("drivers_config", "browser"):
+        browser = config.get("drivers_config", "browser")
+    else:
+        browser = "chrome"
     env = read_configuration().read()
     arr_stage = env.get_list_stage()
     for stage_config in arr_stage:
@@ -33,7 +41,7 @@ def before_all(context):
             arr_device = stage_config.get_list_devices()
             for device in arr_device:
                 if platform == "WEB" and device.get_platform_name() == platform:
-                    launch_browser(context, device)
+                    launch_browser(context, device, browser)
                     break
                 elif platform == "ANDROID" and device.get_platform_name() == platform:
                     print("android")
@@ -51,17 +59,22 @@ def before_all(context):
     context.dict_yaml = ManagementFile().get_dict_path_yaml()
 
 
-def launch_browser(context, device):
-    chrome_option = Options()
-    chromedriver_autoinstaller.install()
-    if device.get_is_headless():
-        chrome_option.add_argument("--headless")
+def launch_browser(context, device, browser):
+    option = get_option_from_browser(browser, device)
     if device.get_auto_download_driver() is False:
-        context.driver = webdriver.Chrome(
-            executable_path=os.path.dirname(os.path.dirname(__file__)) + "\\" + device.get_driver_from_path(),
-            options=chrome_option)
+        get_driver_from_path(context, browser, device, option)
     else:
-        context.driver = webdriver.Chrome(options=chrome_option)
+        if browser == "chrome":
+            chromedriver_autoinstaller.install()
+            context.driver = webdriver.Chrome(options=option)
+        elif browser == "firefox":
+            context.driver = webdriver.Firefox(options=option)
+        elif browser == "safari":
+            context.driver = webdriver.safari(options=option)
+        else:
+            logging.info("Framework only is support for chrome, firefox and safari..., trying open with chrome")
+            chromedriver_autoinstaller.install()
+            context.driver = webdriver.Chrome(options=option)
     context.wait = device.get_wait()
     context.device = device
     context.time_page_load = device.get_time_page_load()
@@ -94,3 +107,44 @@ def after_step(context, step):
 
 def before_scenario(context, scenario):
     logging.info(f'Scenario Name started: {scenario.name}')
+def after_all(context):
+    logging.info("Scenario Name end")
+    if context.driver is not None:
+        context.driver.close()
+        context.driver.quit()
+
+def get_driver_from_path(context, browser, device, option):
+    # //change due to update form selenium 4.10.0 , removed executable_path
+    # https://github.com/SeleniumHQ/selenium/commit/9f5801c82fb3be3d5850707c46c3f8176e3ccd8e
+    if browser == "chrome":
+        service = chrome_service(executable_path=os.path.dirname(os.path.dirname(__file__)) + "\\" + device.get_driver_from_path())
+        context.driver = webdriver.Chrome(service=service, options=option)
+    elif browser == "firefox":
+        service = firefox_service(executable_path=os.path.dirname(os.path.dirname(__file__)) + "\\" + device.get_driver_from_path())
+        context.driver = webdriver.Firefox(service=service, options=option)
+    elif browser == "safari":
+        service = safari_service(
+            executable_path=os.path.dirname(os.path.dirname(__file__)) + "\\" + device.get_driver_from_path())
+        context.driver = webdriver.Safari(service=service, options=option)
+    else:
+        logging.info("Framework only is support for chrome, firefox and safari..., trying open with chrome")
+        service = chrome_service(
+        executable_path=os.path.dirname(os.path.dirname(__file__)) + "\\" + device.get_driver_from_path())
+        context.driver = webdriver.Chrome(service=service, options=option)
+
+
+
+
+def get_option_from_browser(browser, device):
+    if browser == "chrome":
+        option = chrome_option()
+    elif browser == "firefox":
+        option = firefox_option()
+    elif browser == "safari":
+        option = safari_option()
+    else:
+        logging.info("Framework only is support for chrome, firefox and safari..., get option from chrome")
+        option = chrome_option()
+    if device.get_is_headless():
+        option.add_argument("--headless")
+    return option
