b4e060c143a0a905f0bff95cdfb67d05a3bb7ed2
Thang Le <quangthangle1402@gmail.com>
2023-08-14 23:12:21 +0700

    add platform name into project runner
    updated

--- project_runner.py
+++ project_runner.py
@@ -3,12 +3,9 @@ import datetime
 import logging
 import subprocess
 import time
-
 import click
 from bdd_tags_processor.bdd_tags_expression_processor import filter_feature_and_scenarios
-
 from package_installer import ensure_package_versions
-
 logging.basicConfig(level=logging.INFO)
 import os
 
@@ -27,9 +24,11 @@ def main(context):
               help='number of processes. Default value is 5')
 @click.option('--stage', '-sg', 'stage_name', type=click.Choice(['QA', 'SIT', 'UAT', 'PROD']), default='QA',
               help='specify the stage to run. Default value is QA')
+@click.option('--platform', '-pl', 'platform_name', type=click.Choice(['WEB', 'ANDROID', 'iOS', 'API']), default='WEB',
+              help='specify platform to run. Default value is WEB')
 @click.option('--parallel-scheme', '-ps', 'parallel_scheme', type=click.Choice(['feature', 'scenario']),
               default='scenario', help='specify the stage to run. Default value is scenario')
-def run(feature_dir, tags, forks, stage_name, parallel_scheme):
+def run(feature_dir, tags, forks, stage_name, platform_name, parallel_scheme):
     # ensure all the packages are installed
     ensure_package_versions()
 
@@ -48,10 +47,10 @@ def run(feature_dir, tags, forks, stage_name, parallel_scheme):
     }
 
     if total_scenarios > 0:
-        _run_feature(args, stage_name)
+        _run_feature(args, stage_name, platform_name)
 
 
-def config_from_command_line(stage_name):
+def config_from_command_line(stage_name, platform_name):
     project_folder = os.path.dirname(os.path.abspath(__file__))
 
     config = configparser.ConfigParser()
@@ -59,6 +58,7 @@ def config_from_command_line(stage_name):
 
     config.set('project_folder', 'project_folder', project_folder)
     config.set('drivers_config', 'stage', stage_name)
+    config.set('drivers_config', 'platform', platform_name)
 
     # Save the changes to the config.ini file
     with open('config_env.ini', 'w') as config_file:
@@ -66,8 +66,8 @@ def config_from_command_line(stage_name):
     logging.info('Config file updated based on user provided command line arguments')
 
 
-def _run_feature(args, stage_name):
-    config_from_command_line(stage_name)
+def _run_feature(args, stage_name, platform_name):
+    config_from_command_line(stage_name, platform_name)
     cmd = f"behavex {args['params']}"
     logging.info(f'Command prepared: {cmd}')
     # Get the start time in seconds since the epoch
