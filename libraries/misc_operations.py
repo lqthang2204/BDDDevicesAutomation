e9a48a0c35a74fe962d278a18d7737f96d203e8a
QA Rampage <16265665+qarampage@users.noreply.github.com>
2023-08-16 17:29:23 +0530

    not to fail when payload DataTable is not provided

--- libraries/misc_operations.py
+++ libraries/misc_operations.py
@@ -1,6 +1,8 @@
-def sanitize_datatable(con_table):
+def sanitize_datatable(context_table):
     # Using list comprehension to create the context_table
-    context_table = [[value.strip() if value.strip() else None for value in row.cells]
-                     for row in con_table]
+    con_table = []
+    if context_table:
+        con_table = [[value.strip() if value.strip() else None for value in row.cells]
+                         for row in context_table]
 
-    return context_table
+    return con_table
